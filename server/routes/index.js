40f1dd9c76ea8d90c0e4fa7b6d9272853da07b4d
Mudit Mathur <muditmathur@Mudits-MacBook-Air.local>
2024-07-25 21:48:02 -0400

    Initial Commit: editing index page welcome messages

--- server/routes/index.js
+++ server/routes/index.js
@@ -25,18 +25,20 @@ router.get('/', async (req, res, next) => {
         if (!context) {
             return res.render('index', {
                 isZoom: false,
-                title: `Hello Browser`,
+                title: `Welcome to Naptime!`,
             });
         }
 
         // Check if the context is valid and not expired
         if (isContextExpired(context)) {
-            return res.status(401).json({ error: 'Invalid or expired context' });
+            return res
+                .status(401)
+                .json({ error: 'Invalid or expired context' });
         }
 
         return res.render('index', {
             isZoom: true,
-            title: `Hello Zoom`,
+            title: `Naptime`,
         });
     } catch (e) {
         next(handleError(e));
